12f50fbc50bb05cc6e699901deb27fd7b52d409b
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2020-07-14 20:24:40 -0400

    Fixed crash when WorldGuard WorldContainer returned null

--- src/com/thizthizzydizzy/treefeller/compat/WorldGuardCompat2.java
+++ src/com/thizthizzydizzy/treefeller/compat/WorldGuardCompat2.java
@@ -17,6 +17,9 @@ public class WorldGuardCompat2{
         boolean canBypass = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(lp, lp.getWorld());
         if(!canBypass){
             com.sk89q.worldguard.protection.regions.RegionContainer container = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getRegionContainer();
+            if(container==null){//RegionContainer is null. I don't know what this means, probably that there's no regions?
+                return null;//all is good, no regions(?)
+            }
             com.sk89q.worldguard.protection.regions.RegionQuery query = container.createQuery();
             for(Block b : blocks){
                 com.sk89q.worldedit.util.Location loc = new com.sk89q.worldedit.util.Location(lp.getWorld(), b.getX(), b.getY(), b.getZ());
