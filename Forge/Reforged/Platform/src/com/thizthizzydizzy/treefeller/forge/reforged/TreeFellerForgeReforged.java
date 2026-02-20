a71136ad1faf3f669b71ba0de6b2cc9b333dad9a
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2026-02-19 23:07:55 -0500

    switched to slf4j for logging on reforged

--- Forge/Reforged/Platform/src/com/thizthizzydizzy/treefeller/forge/reforged/TreeFellerForgeReforged.java
+++ Forge/Reforged/Platform/src/com/thizthizzydizzy/treefeller/forge/reforged/TreeFellerForgeReforged.java
@@ -1,7 +1,10 @@
 package com.thizthizzydizzy.treefeller.forge.reforged;
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
 @Mod(TreeFellerForgeReforged.MOD_ID)
 public class TreeFellerForgeReforged{
     public static final String MOD_ID = "treefeller";
+    public static final Logger LOGGER = LogUtils.getLogger();
     public static ForgeConnector connector;
 }
